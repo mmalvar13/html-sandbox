cfcb362a88587865e3dfe8985c2aedb235db1a99
Monica Alvarez <mmalvar13@gmail.com>
2016-07-12 12:51:52 -0600

    added paragraphs and lists

--- index.php
+++ index.php
@@ -6,5 +6,23 @@
 	</head>
 	<body>
 		<h1>Hai World!</h1>
+		<p>Soft kitty warm kitty little ball of furr lick sellotape or damn that dog for spread kitty litter all over house so wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again so Gate keepers of hell and climb leg. Human give me attention meow damn that dog . Hola te quiero plan steps for world domination so see owner, run in terror. Thug cat jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed for make muffins under the bed, or unwrap toilet paper destroy the blinds, lick arm hair. Stand in front of the computer screen lounge in doorway under the bed lick yarn hanging out of own butt and plan steps for world domination. </p>
+		<h2>About Me</h2>
+		<p>Soft kitty warm kitty little ball of furr lick sellotape or damn that dog for spread kitty litter all over house so wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again so Gate keepers of hell and climb leg. Human give me attention meow damn that dog . Hola te quiero plan steps for world domination so see owner, run in ter</p>
+		<ul>
+			<li>item 1</li>
+			<li>item 2</li>
+			<li>item 3</li>
+		</ul>
+		<ol>
+			<li>item 1</li>
+			<li>item 2</li>
+			<li>item 3</li>
+		</ol>
+
+		<div>
+			<a href="https://google.com" target="_blank">Go to Google.com</a>
+		</div>
+
 	</body>
 </html>
