6b61303c7ea4ce4f8328d8a73c36bc5047281f57
Monica Alvarez <mmalvar13@gmail.com>
2016-07-12 15:08:32 -0600

    added comments

--- index.php
+++ index.php
@@ -5,9 +5,11 @@
 		<title>My Web Page</title>
 	</head>
 	<body>
+
 		<header>
 			<h1>Hai World!</h1>
 		</header>
+		<!--Start Main Content Here-->
 		<main>
 			<p id="first-paragraph">Soft kitty <strong>warm kitty</strong> little <em>ball of furr</em> lick sellotape or damn that dog for spread kitty litter all over house so wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again so Gate keepers of hell and climb leg. Human give me attention meow damn that dog . Hola te quiero plan steps for world domination so see owner, run in terror. Thug cat jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed for make muffins under the bed, or unwrap toilet paper destroy the blinds, lick arm hair. Stand in front of the computer screen lounge in doorway under the bed lick yarn hanging out of own butt and plan steps for world domination. </p>
 			<h2 class="pretty-title">About Me</h2>
