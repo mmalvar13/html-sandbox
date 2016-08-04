2fa548d2cde483366db8b6d9663e2a7f8bfa8c2e
Monica Alvarez <mmalvar13@gmail.com>
2016-08-04 08:53:25 -0600

    sandbox project pre-reset

--- index.php
+++ index.php
@@ -19,8 +19,12 @@
 		<!--Start Main Content Here-->
 		<main>
 			<p id="first-paragraph">Soft kitty <strong>warm kitty</strong> little <em>ball of furr</em> lick sellotape or damn that dog for spread kitty litter all over house so wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again so Gate keepers of hell and climb leg. Human give me attention meow damn that dog . Hola te quiero plan steps for world domination so see owner, run in terror. Thug cat jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed for make muffins under the bed, or unwrap toilet paper destroy the blinds, lick arm hair. Stand in front of the computer screen lounge in doorway under the bed lick yarn hanging out of own butt and plan steps for world domination. </p>
-			<h2 class="pretty-title">About Me</h2>
+			<h2 class="pretty-title red text">About Me</h2>
 			<p>Soft kitty warm kitty little ball of furr lick sellotape or damn that dog for spread kitty litter all over house so wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again so Gate keepers of hell and climb leg. Human give me attention meow damn that dog . Hola te quiero plan steps for world domination so see owner, run in ter</p>
+			<h1 class="pretty-title red-text">Another Heading</h1>
+			<p>p wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again so Gate keepers </p>
+			<h1 class="pretty-title red-text">Another Heading</h1>
+			<p>eep again so Gate keepers of hell and climb leg. Human give me attention meow damn  again so Gate keepers of hell and climb leg. Human give me attention meow damn that dog . Hola te quiero plan steps for world domination so</p>
 			<ul>
 				<li>item 1</li>
 				<li>item 2</li>
